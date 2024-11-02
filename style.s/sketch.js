d9f6a5f0c1fa9ba2ebd78a1134a47e5762106dee
Micaelazangolo <micaelamabiala@gmail.com>
2024-11-02 11:56:17 -0300

    adicionado arquivo js.css

---
+++ style.s/sketch.js
@@ -0,0 +1,11 @@
+
+function sayHello() {
+    // Use prompt to get user input.
+    let name = prompt('Enter your name:');
+
+    // Use console.log to display results.
+    console.log('Hello, ' + name + '!');
+}
+
+// Press the play button to see the result.
+sayHello();
