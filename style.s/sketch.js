1c71627fe91dc98fda323726a0a7fa37a4e7b118
Micaelazangolo <micaelamabiala@gmail.com>
2024-11-02 09:59:13 -0300

    adicionado arquivo js .css

---
+++ style.s/sketch.js
@@ -0,0 +1,19 @@
+funcao inicio ()
+	{
+		
+		inteiro numero[7]={12,13,25,33,37,48,50}
+		
+		escreva("lista de número de relógio:\n")
+		
+		inteiro i=1	
+		
+		enquanto (i<7)
+		{	
+			escreva (numero[i]+"\n")
+			i = i+2
+		}
+		
+		
+		
+	}
+}
